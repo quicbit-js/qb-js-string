161d23131c4beb5234b0474c97ff474dbcf3ad0c
mvoss9000 <mvoss9000@yahoo.com>
2017-12-01 14:02:20 -0500

    place comma next to column data

--- index.js
+++ index.js
@@ -13,9 +13,37 @@
 // WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-'use strict'
 
-function jstr (v) {
+var assign = require('qb-assign')
+
+// options-by-depth.
+var DEFAULT_OPT = [
+  // igap is initial gap (next to brace) [ <here> val1, val2 <here> ]
+  // gap is gap between values [ val1,<here>val2 ] - gets smaller at greater depths
+  {igap: ' ', gap: ' '},  // depth  0
+  {igap: '',  gap: ' '},   // depth  1
+  {igap: '',  gap: ''},    // depth >1
+]
+
+function init_opt (opt) {
+  var ret
+  if (opt) {
+    // use defaults to fill missing options at every depth.
+    ret = Array.isArray(opt) ? opt : [opt]
+    ret = ret.map(function (o, i) {
+      return assign({}, DEFAULT_OPT[i] || DEFAULT_OPT[DEFAULT_OPT.length-1], o)
+    })
+  } else {
+    ret = DEFAULT_OPT
+  }
+  return ret
+}
+
+function jstr (v, opt) {
+  return _jstr(v, init_opt(opt), 0)
+}
+
+function _jstr (v, optarr, depth) {
   if (v === null) {
     return 'null'
   } else if (v === undefined) {
@@ -23,7 +51,7 @@ function jstr (v) {
   } else {
     switch (typeof v) {
       case 'object':
-        return Array.isArray(v) ? arr2str(v) : obj2str(v)
+        return Array.isArray(v) ? arr2str(v, optarr, depth) : obj2str(v, optarr, depth)
       case 'string':
         return str2str(v)
       case 'number' :
@@ -34,21 +62,88 @@ function jstr (v) {
     }
   }
 }
+
+// simple flex-spacing algorithm prioritizes left-most column alignment and when
+// exceeded, attempts to recover alignment on subsequent columns.  So when arranging
+// test data, placing smaller columns to the left to creates better fit.
+// So the position of col2 and col3 below represent the max point that accomodated all
+// qualifying data.  Rows labled '1&2' fit col1 and col2 and exceeded (40) at
+// col3.  Rows '1&3' failed to fit col2, but fit again at col3 and were used to
+// align that column.
+// (max_col: 20)
+//
+//        0                   20                  40                  60
+//        |                   |                   |                   |
+//      [ 'col1',       'col2',                'col3' ],
+// 123  [ 'short',      'medium_len',          'long long long long long' ],
+// 123  [ 'short',      'medium_len',          'long long long long long' ],
+// 1&2  [ 'medium_len', 'long long long long long long', 'short' ],
+// 1&2  [ 'medium_len', 'long long long long long long', 'short' ],
+// 123  [ 'short',      'short',               'long long long long long' ],
+// 123  [ 'short',      'medium_len',          'long long long long long' ],
+// 1&3  [ 'long long long long long', 'short', 'long long long long long' ],
+// 1&3  [ 'long long long long long', 'short', 'medium len' ],
+//
+function arr2str (a, optarr, depth) {
+  if (a.length === 0) { return '[]' }
+  var opt = optarr[depth] || optarr[optarr.length-1]
+  var last = a.length - 1
+  var ret = a.map(function (v, i) { return _jstr(v, optarr, depth + 1) + (i < last ? ',' : '') })
+  return '[' + opt.igap + ret.join(opt.gap) + opt.igap + ']'
+}
+
+function obj2str (o, optarr, depth) {
+  var keys = Object.keys(o)
+  if (keys.length === 0) { return '{}' }
+  var pairs = keys.map(function (k) { return key2str(k) + ': ' + _jstr(o[k], optarr, depth + 1) })
+  return '{ ' + pairs.join(', ') + ' }'
+}
+
+function is_table (a) {
+  if (a.length < 2) { return false }
+  var len = a[0].length
+  return !a.find(function (v) {
+    return !v || (v.length && v.length !== len) || !Array.isArray(v)
+  })
+}
+
+function padr (s, l, c) { while (s.length < l) s = s + c; return s }
+
+function table_rows (a, opt) {
+  var cell_strings = a.map(function (row) { return row.map(function (v) { return _jstr(v, init_opt(opt), 0) }) })
+  var numcols = cell_strings[0].length
+  var widths = []; for (var i = 0; i < numcols; i++) { widths[i] = 0 }
+
+  cell_strings.forEach(function (row) {
+    row.forEach(function (s, i) {
+      var slen = s.length + (i < numcols.length - 1 ? 1 : 0)    // + 1 for comma
+      if (slen > widths[i]) { widths[i] = slen }
+    })
+  })
+
+  return cell_strings.map(function (row) {
+    var padded = row.map(function (s, i) {
+      // add commas next to data (less cluttered and easier to manage by hand later)
+      if (i < row.length - 1) { s += ',' }
+      return padr(s, widths[i], ' ')
+    })
+    return '[ ' + padded.join(' ') + ' ],'
+  })
+}
+
+function table (a, opt) {
+  is_table(a) || err('data is not a table (array of 2 or more same-length arrays)')
+  return table_rows(a, opt).join('\n')
+}
+
 // escape logic, especially the regex, is based on logic in http://github.com/douglascrockford/JSON-js,
-// with additional characters from ECMA-262.
-
-// ECMA-262 line terminators
-// \u000A Line Feed <LF>
-// \u000D Carriage Return <CR>
-// \u2028 Line separator <LS>
-// \u2029 paragraph sep <PS>
-
-// ECMA 262 other mentions (not lised in slash_esc)
-// \u00A0   no-break space
-// \uFEFF   byte order mark
-// \u0020   space (not escaped)
-// PLUS - any unicode whitespace
-
+// with additional characters from ECMA-262:
+// line terminators: \u000A <LF> \u000D <CR> \u2028 <LS> \u2029 <PS>
+// other mentions (not listed in slash_esc)
+//    \u00A0   no-break space
+//    \uFEFF   byte order mark
+//    \u0020   space (not escaped)
+//    PLUS - any unicode whitespace
 var slash_esc = {
   '\b': '\\b',      //  \u0008   <BS> backspace
   '\t': '\\t',      //  \u0009   <HT> horizontal tab
@@ -91,7 +186,7 @@ function str2str (s) {
         q = '"'
       }
       if (d_qt) {
-                // string has single and double quotes - escape the chosen quote (least-bad)
+        // string has single and double quotes - escape the chosen quote (least-bad)
         s = s.replace(quote_re[q], qt_esc[q])
       }
     }
@@ -101,55 +196,6 @@ function str2str (s) {
 
 function key2str (k) {
   return (k.match(/^[_$a-zA-Z][_$a-zA-Z0-9]*$/)) ? k : str2str(k)
-}
-
-function arr2str (a) {
-  if (a.length === 0) { return '[]' }
-  var ret = a.map(function (v) { return jstr(v) })
-  return '[ ' + ret.join(', ') + ' ]'
-}
-
-function obj2str (o) {
-  var keys = Object.keys(o)
-  if (keys.length === 0) { return '{}' }
-  var pairs = keys.map(function (k) { return key2str(k) + ': ' + jstr(o[k]) })
-  return '{ ' + pairs.join(', ') + ' }'
-}
-
-function is_table (a) {
-  if (a.length < 2) { return false }
-  var len = a[0].length
-  return !a.find(function (v) {
-    return !v || (v.length && v.length !== len) || !Array.isArray(v)
-  })
-}
-
-function padr (s, l, c) { while (s.length < l) s = s + c; return s }
-
-function table_rows (a) {
-  var cell_strings = a.map(function (row) { return row.map(function (v) { return jstr(v) }) })
-  var header = cell_strings[0]
-  var widths = header.map(function (h) { return h.length + 1 }) // + 1 for comma
-
-  cell_strings.forEach(function (row) {
-    row.forEach(function (s, i) {
-      if (s.length > widths[i]) { widths[i] = s.length + 1 }  // + 1 for comma
-    })
-  })
-
-  return cell_strings.map(function (row) {
-    var padded = row.map(function (s, i) {
-      // add commas next to data (less cluttered and easier to manage by hand later)
-      if (i < row.length - 1) { s += ',' }
-      return padr(s, widths[i], ' ')
-    })
-    return '[ ' + padded.join(' ') + ' ],'
-  })
-}
-
-function table (a) {
-  is_table(a) || err('data is not a table (array of 2 or more same-length arrays)')
-  return table_rows(a).join('\n')
 }
 
 function err (msg) { throw Error(msg) }
--- test.js
+++ test.js
@@ -18,75 +18,100 @@ var test = require('test-kit').tape()
 var jstr = require('.')
 
 test('jstr() literals', function (t) {
-    t.table_assert([
-        [  'v',              'exp' ],
-        [    '',               "''" ],
-        [ 'abc',            "'abc'" ],
-        [ "Bill's",         "\"Bill's\"" ],
-        [ "\"Bill's\"",      "'\"Bill\\'s\"'" ],
+  t.table_assert([
+    [  'v',                       'exp' ],
+    [    '',                      "''" ],
+    [ 'abc',                      "'abc'" ],
+    [ "Bill's",                   "\"Bill's\"" ],
+    [ "\"Bill's\"",               "'\"Bill\\'s\"'" ],
 
-        [ 'a\n',            "'a\\n'" ],
-        [ 'a\\b',            "'a\\\\b'" ],
-        [ 'x\u2028y\u2029\u2030', "'x\\u2028y\\u2029‰'" ],
-        [     9,                "9" ],
-        [ 3.245,            "3.245" ],
-        [ true,             "true"  ],
-        [ false,           "false"  ],
-        [  null,             "null" ],
-    ], jstr )
+    [ 'a\n',                      "'a\\n'" ],
+    [ 'a\\b',                     "'a\\\\b'" ],
+    [ 'x\u2028y\u2029\u2030',     "'x\\u2028y\\u2029‰'" ],
+    [ 9,                          "9" ],
+    [ 3.245,                      "3.245" ],
+    [ true,                       "true"  ],
+    [ false,                      "false"  ],
+    [  null,                      "null" ],
+  ], jstr )
 })
 
 test('jstr() errors',          function (t) {
-    t.table_assert([
-        [ 'v',                      'exp' ],
-        [ function () {},           /type "function" not implemented/ ],
-    ], jstr, {assert: 'throws'} )
+  t.table_assert([
+    [ 'v',                      'exp' ],
+    [ function () {},           /type "function" not implemented/ ],
+  ], jstr, {assert: 'throws'} )
 })
 
-test('jstr() array - single', function (t) {
-    t.table_assert([
-        [ 'a',                      'exp' ],
-        [ [],                       "[]" ],
-        [ [1],                      "[ 1 ]" ],
-        [ ['a'],                    "[ 'a' ]" ],
-        [ [9],                      "[ 9 ]" ],
-        [ [1,2,null,"",'x'],        "[ 1, 2, null, '', 'x' ]"],
-    ], jstr)
+test('jstr() array', function (t) {
+  t.table_assert([
+    [ 'a',                'opt',                  'exp' ],
+    [ [],                 null,                   "[]" ],
+    [ [1],                null,                   "[ 1 ]" ],
+    [ [1],                {igap: ''},             "[1]" ],
+    [ ['a'],              null,                   "[ 'a' ]" ],
+    [ [9],                null,                   "[ 9 ]" ],
+    [ [1,2,null,"",'x'],  {igap: '', gap: ''},    "[1,2,null,'','x']"],
+    [ [1,2,null,"",'x'],  {igap: ' ', gap: ''},   "[ 1,2,null,'','x' ]"],
+    [ [1,2,null,"",'x'],  {igap: ' ', gap: ' '},  "[ 1, 2, null, '', 'x' ]"],
+    [ [1,2,null,"",'x'],  null,                   "[ 1, 2, null, '', 'x' ]"],
+    [ [1,[2,3],[4,5,6]],  null,                   '[ 1, [2, 3], [4, 5, 6] ]' ],
+    [ [1,[2,[3,4,5,6]]],  null,                   '[ 1, [2, [3,4,5,6]] ]' ],
+  ], jstr)
 })
 
 test('jstr() object', function (t) {
-    t.table_assert([
-        [ 'a',                              'exp' ],
-        [ {},                               "{}" ],
-        [ {'a':2},                          "{ a: 2 }" ],
-        [ {'a':{'b':7}},                    "{ a: { b: 7 } }" ],
-        [ {'a':null, 'b': undefined },      "{ a: null, b: undefined }" ],
-        [ { '&q': true },                   "{ '&q': true }" ],
-    ], jstr)
+  t.table_assert([
+    [ 'a',                              'exp' ],
+    [ {},                               "{}" ],
+    [ {'a':2},                          "{ a: 2 }" ],
+    [ {'a':{'b':7}},                    "{ a: { b: 7 } }" ],
+    [ {'a':null, 'b': undefined },      "{ a: null, b: undefined }" ],
+    [ { '&q': true },                   "{ '&q': true }" ],
+  ], jstr)
 })
 
 test('jstr.table_rows()', function (t) {
-    t.table_assert([
-        [ 'tbl',                                    'exp' ],
-        [ [['a'], [1], [2]],                        [ "[ 'a' ],", "[ 1   ],", "[ 2   ]," ] ],
-        [ [['a','b'], [1,'x'], [2,'y']],            [ "[ 'a', 'b' ],", "[ 1  , 'x' ],", "[ 2  , 'y' ]," ] ],
-        [ [['a','b'], [1,['x', 4]], [2,['y',5]]],   [ "[ 'a', 'b'        ],", "[ 1  , [ 'x', 4 ] ],", "[ 2  , [ 'y', 5 ] ]," ] ],
-    ], jstr.table_rows)
+  t.table_assert([
+    [ 'tbl',                                    'exp' ],
+    [ [['a'], [1], [2]],                        [ "[ 'a' ],", "[ 1   ],", "[ 2   ]," ] ],
+    [ [['a','b'], [1,'x'], [2,'y']],            [ "[ 'a', 'b' ],", "[ 1,  'x' ],", "[ 2,  'y' ]," ] ],
+    [ [['a','b'], [1,['x', 4]], [2,['y',5]]],   [ "[ 'a', 'b'        ],", "[ 1,  [ 'x', 4 ] ],", "[ 2,  [ 'y', 5 ] ]," ] ],
+  ], jstr.table_rows)
 })
 
 test('jstr.table()', function (t) {
-    t.table_assert([
-        [ 'tbl',                                    'exp' ],
-        [ [['a'], [1], [2]],                        "[ 'a' ],\n[ 1   ],\n[ 2   ]," ],
-        [ [['a','b'], [1,'x'], [2,'y']],            "[ 'a', 'b' ],\n[ 1  , 'x' ],\n[ 2  , 'y' ]," ],
-        [ [['a','b'], [1,['x', 4]], [2,['y',5]]],   "[ 'a', 'b'        ],\n[ 1  , [ 'x', 4 ] ],\n[ 2  , [ 'y', 5 ] ]," ],
-    ], jstr.table)
+  t.table_assert([
+    [ 'tbl',                                    'exp' ],
+    [
+      [['a'], [1], [2]],
+      t.lines(`
+        [ 'a' ],
+        [ 1   ],
+        [ 2   ],`
+      ).join('\n')
+    ],
+    [ [['a','b'], [1,'x'], [2,'y']],
+      t.lines(`
+        [ 'a', 'b' ],
+        [ 1,  'x' ],
+        [ 2,  'y' ],
+      `).join('\n')
+    ],
+    [ [['a','b'], [1,['x', 4]], [2,['y',5]]],
+      t.lines(`
+        [ 'a', 'b'        ],
+        [ 1,  [ 'x', 4 ] ],
+        [ 2,  [ 'y', 5 ] ],
+      `).join('\n')
+    ],
+  ], jstr.table)
 })
 
 test('jstr.table() errors', function (t) {
-    t.table_assert([
-        [ 'tbl',                                    'exp' ],
-        [ [['a'], [1, 2]],                          /data is not a table/ ],
-        [ [['a', 'b', 'c']],                        /data is not a table/ ],
-    ], jstr.table, {assert: 'throws'})
+  t.table_assert([
+    [ 'tbl',                                    'exp' ],
+    [ [['a'], [1, 2]],                          /data is not a table/ ],
+    [ [['a', 'b', 'c']],                        /data is not a table/ ],
+  ], jstr.table, {assert: 'throws'})
 })
